3c4347500251917028805ec0259cc29545acfcc9
Victoria Bochulya <v.bochulya@yandex.ru>
2024-11-13 22:56:47 +0300

    Изучила шшаблон, сделала открытие формы

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,14 +1,23 @@
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
+import { Text } from 'src/ui/text'
 
 import styles from './ArticleParamsForm.module.scss';
+import { useState } from 'react';
+
+import clsx from 'clsx';
 
 export const ArticleParamsForm = () => {
+
+	//Состояние для отслеживания открытия/закрытия панели
+	const [isOpen, setOpen] = useState<boolean>(false)
+
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
+			<ArrowButton isOpen={isOpen} onClick={() => setOpen(!isOpen)} />
+			<aside className={clsx(styles.container, isOpen && styles.container_open)}>
 				<form className={styles.form}>
+					<Text>Задайте параметры</Text>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
 						<Button title='Применить' htmlType='submit' type='apply' />
